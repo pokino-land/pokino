2f972d6c209891eca2f51ccca42de7b92c872e8a
Matthäus Heer <matthaeusheer@gmail.com>
2021-04-01 21:56:26 +0200

    Correct jdbc sample with row mapper.

--- backend/demo/src/main/java/ch/uzh/pokino/demo/DemoApplication.java
+++ backend/demo/src/main/java/ch/uzh/pokino/demo/DemoApplication.java
@@ -2,12 +2,9 @@ package ch.uzh.pokino.demo;
 
 import ch.uzh.pokino.demo.config.DbConfig;
 import ch.uzh.pokino.demo.config.DemoConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @Import({DemoConfig.class, DbConfig.class})
@@ -15,16 +12,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate){
-
-		String QUERY = "SELECT count(*) FROM T_ACCOUNT";
-
-		// Use Lambda expression to display the result
-		return args -> System.out.println("Hello, there are "
-				+ jdbcTemplate.queryForObject(QUERY, Long.class)
-				+ " accounts");
 	}
 }
--- backend/demo/src/main/java/ch/uzh/pokino/demo/accounts/JdbcAccountManager.java
+++ backend/demo/src/main/java/ch/uzh/pokino/demo/accounts/JdbcAccountManager.java
@@ -1,24 +1,47 @@
 package ch.uzh.pokino.demo.accounts;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.AbstractCollection;
 import java.util.List;
 
 @Component
-@EnableAutoConfiguration
 public class JdbcAccountManager implements AccountManager {
 
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-    public JdbcAccountManager(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public JdbcAccountManager(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    private static class AccountRowMapper implements RowMapper<Account> {
+        public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return mapAccount(rs);
+        }
+
+        private Account mapAccount(ResultSet rs) throws SQLException {
+            // get the row column data
+            String name = rs.getString("NAME");
+            String number = rs.getString("NUMBER");
+
+            // map to the object
+            return new Account(number, name);
+        }
+    }
 
     @Override
     public List<Account> getAllAccounts() {
-        return jdbcTemplate.queryForList("select * from T_ACCOUNT", Account.class);
+        AccountRowMapper rowMapper = new AccountRowMapper();
+        var ret = jdbcTemplate.query("select * from T_ACCOUNT", rowMapper);
+        return ret;
+    }
+
+    @Override
+    public Long getNumberOfAccounts() {
+        return jdbcTemplate.queryForObject("select count(*) from T_ACCOUNT", Long.class);
     }
 }
