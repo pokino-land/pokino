2f9b6b16e76353cc68a1e7b83e96021e61084607
StevenSchuerstedt <50596774+StevenSchuerstedt@users.noreply.github.com>
2021-05-18 10:56:02 +0200

    simple tests for render classes (#103)
    * simple tests for render classes
    
    * formatting

---
+++ frontend/pokino-app/src/app/model/render/physics.spec.ts
@@ -0,0 +1,26 @@
+import { physics } from '../render/physics';
+
+describe('physics', () => {
+  const width = 10;
+  const height = 10;
+  let test_physics = new physics(width, height);
+
+
+  it('should have width', () => {
+    expect(test_physics.m_sceneWidth).toBe(width);
+  });
+
+  it('should have height', () => {
+    expect(test_physics.m_sceneHeight).toBe(height);
+  });
+
+
+  it('should have a enemy body', () => {
+    expect(test_physics.enemy).toBeDefined();
+  });
+
+  it('should have a mesh', () => {
+    expect(test_physics.ball).toBeDefined();
+  });
+
+});
