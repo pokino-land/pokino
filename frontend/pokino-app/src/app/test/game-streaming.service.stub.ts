09cc05377378c1b856363480f8350a34773f8672
Leo-Rutschmann <45575636+Leo-Rutschmann@users.noreply.github.com>
2021-05-30 18:47:25 +0200

    set up code coverage for the frontend (#138)
    * set up code coverage for the frontend
    
    * travis ci test
    
    Co-authored-by: Leo-Rutschmann <leo.rutschmann@gmail.com>
    Co-authored-by: Steven Schürstedt <steven@schuerstedt.com>

---
+++ frontend/pokino-app/src/app/test/game-streaming.service.stub.ts
@@ -0,0 +1,40 @@
+// functionality is tested in game-streaming.service.spec, so these are just fillers to ensure components work
+import {GameStreamingService} from "../view/websocket-adapter/game-streaming.service";
+import {JsonPlayerObject} from "../api/json-player-object";
+import {JsonGameEndsObject} from "../api/json-game-ends-object";
+import {JsonGameInitObject} from "../api/json-game-init-object";
+
+export const gameStreamingServiceStub: Partial<GameStreamingService> = {
+    getWebSocket(): WebSocket {
+        return new WebSocket('localhost://testurl');
+    },
+
+    getGameInitTopic(): string {
+        return 'testTopic';
+    },
+
+    sendGameStartsConfirmation(player: JsonPlayerObject): Promise<void> {
+        return new Promise(() => {});
+    },
+
+    endGame(gameEndsMessage: JsonGameEndsObject): any {
+    },
+
+    initGameIds(gameInitMessage: JsonGameInitObject): void {
+    },
+
+    getGameDownstreamTopic(): string {
+        return 'testTopic';
+    },
+
+    getGameUpstreamTopic(): string {
+        return 'testTopic';
+    },
+
+    getGameShutdownTopic(): string {
+        return 'testTopic';
+    },
+
+    sendBallThrown(didHit: boolean): void {
+    }
+};
